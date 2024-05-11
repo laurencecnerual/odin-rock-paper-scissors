76acd0c2b5880c9c3cc45542c99d93ad75718556
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-05-11 14:09:40 +0900

    Adds logic for getHumanChoice()
    Also refactors to add generic choiceToString(), Adds comments

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+// Borrowed from mdn web docs - Returns a random integer between min and max, inclusive
 function getRandomIntInclusive(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
+// Returns the computer's Rock-Paper-Scissor choice as a string (based on random number generation)
 function getComputerChoice() {
-    let choice = getRandomIntInclusive(1, 3);
+    return choiceToString(getRandomIntInclusive(1, 3)); 
+}
 
+// Returns the user's Rock-Paper-Scissor choice as a string (based on user input)
+function getHumanChoice() {
+    return choiceToString(parseInt(prompt("Please choose a number between 1 and 3, where 1 = Rock, 2 = Paper, and 3 = Scissors")));
+}
+
+// Takes a numerical respresentation of choice and returns its Rock-Paper-Scissor equivalent
+function choiceToString(choice) {
     if (choice == 1) {
         return "Rock";
     } else if (choice == 2) {
@@ -14,6 +24,6 @@ function getComputerChoice() {
     } else if (choice == 3) {
         return "Scissors";
     } else {
-        return undefined;
+        return "Unexpected Choice";
     }
 }
